7e580e6182fc48363ae0cb5accbf436675f3f39c
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-05 16:22:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,10 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-array.collect 
+array= string.split(" ")
+   new_array=[]
+   array.collect do |string| 
+  new_array<<string.reverse
+ end
+  new_array.join(" ")
+end
