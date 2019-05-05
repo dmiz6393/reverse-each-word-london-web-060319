3f667bef876110f13b34169f81f6f9dc02285525
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-05 16:20:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(sentence1)
-   array= sentence1.split 
+def reverse_each_word(string)
+   array= string.split 
    new_array=[]
-   array.each do |sentence1| 
-  new_array<<sentence1.reverse
+   array.each do |string| 
+  new_array<<string.reverse
  end
   return_array.join
 end
